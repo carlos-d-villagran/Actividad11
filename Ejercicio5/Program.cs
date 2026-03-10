0889b2badd0ba515b805851bf8aa9f6a3d0f19f8
carlos-d-villagran <carlos.danivillagran27@gmail.com>
2026-03-09 19:23:31 -0600

    mas ejercicios

--- Ejercicio5/Program.cs
+++ Ejercicio5/Program.cs
@@ -8,9 +8,18 @@ namespace Ejercicio5
 {
     internal class Program
     {
+        static void MostrarMenu()
+        {
+            Console.WriteLine("++++ Menú de estudiantes ++++");
+            Console.WriteLine("1. Agregar");
+            Console.WriteLine("2. Modificar");
+            Console.WriteLine("3. Desplegar");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+        }
         static void Main(string[] args)
         {
-
+            MostrarMenu();
         }
     }
 }
---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    internal class Program
+    {
+        
+        static void Saludar(string nombre)
+        {
+            Console.WriteLine("Hola " + nombre);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique su nombre");
+            string nombre = Console.ReadLine();
+            Saludar(nombre);
+        }
+    }
+}
